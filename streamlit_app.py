727914329770afe65a5db2da9a0832dc5fd045ba
deepikaguptagithub <145993021+deepikaguptagithub@users.noreply.github.com>
2023-09-25 18:29:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
-#streamlit.title('My parents new healthy diner')
-streamlit.title('Changed title 1')
+streamlit.title('My parents new healthy diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('text 1')
+streamlit.text('text 2')
+streamlit.text('text 3')
